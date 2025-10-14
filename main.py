8081368a81f1285603a05a7470ece9ff5aeb0056
AmaAde <Amalia.Adetu@softwire.com>
2025-10-14 17:15:03 +0300

    implemented rules for 7 and 11

--- main.py
+++ main.py
@@ -5,19 +5,33 @@
 
 
 def fizzbuzz():
-    for i in range(1,101):
+    for i in range(1,500):
+        if i % 11 == 0:
+            print("Bong")
+            continue
+
+        placeHolder = ""
+
         if i % 3 == 0:
             if i % 5 == 0:
-                print("FizzBuzz")
+                placeHolder = "FizzBuzz"
             else:
-                print("Fizz")
+                placeHolder = "Fizz"
         elif i % 5 == 0:
-            print("Buzz")
+            placeHolder = "Buzz"
+        elif i % 7 == 0:
+            placeHolder = "Bang"
+
+        if i % 7 == 0 and (i % 3 == 0 or i % 5 == 0):
+            placeHolder += "Bang"
+
+        if placeHolder:
+            print(placeHolder)
         else:
             print(i)
 
-# Press the green button in the gutter to run the script.
+
+
 if __name__ == '__main__':
     fizzbuzz()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
