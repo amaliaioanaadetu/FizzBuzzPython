8603369f16db3875366deb530c355f5767945af4
AmaAde <Amalia.Adetu@softwire.com>
2025-10-14 15:54:19 +0300

    part 1 done

--- main.py
+++ main.py
@@ -4,13 +4,20 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+def fizzbuzz():
+    for i in range(1,101):
+        if i % 3 == 0:
+            if i % 5 == 0:
+                print("FizzBuzz")
+            else:
+                print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    fizzbuzz()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
