44b673efbe0e2c6bc67ddb9edc01d5eeea3d5abc
AmaAde <Amalia.Adetu@softwire.com>
2025-10-15 09:49:44 +0300

    made function for one-liner fizzbuzz

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # This is a sample Python script.
+import math
+
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -13,16 +15,10 @@ def fizzbuzz(upperLimit):
         placeHolder = ""
 
         if i % 3 == 0:
-            if i % 5 == 0:
-                placeHolder = "FizzBuzz"
-            else:
-                placeHolder = "Fizz"
-        elif i % 5 == 0:
-            placeHolder = "Buzz"
-        elif i % 7 == 0:
-            placeHolder = "Bang"
-
-        if i % 7 == 0 and (i % 3 == 0 or i % 5 == 0):
+            placeHolder += "Fizz"
+        if i % 5 == 0:
+            placeHolder += "Buzz"
+        if i % 7 == 0:
             placeHolder += "Bang"
 
         if i % 13 == 0:
@@ -46,8 +42,12 @@ def fizzbuzz(upperLimit):
             print(i)
 
 
+def fizzbuzzSimple(upperLimit):
+    res = list(map(lambda x: math.floor((x - 1) % 3 / 2) * "Fizz" + math.floor((x - 1) % 5 / 4) * "Buzz" or x, range(1, upperLimit + 1)))
+    for elem in res:
+        print(elem)
 
 if __name__ == '__main__':
-    upperLimit = int(input("Select the maximum number:"))
-    fizzbuzz(upperLimit)
+    upperLimit = int(input("Select the maximum number: "))
+    fizzbuzzSimple(upperLimit)
 
